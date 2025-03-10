f924e0719a7f2ad5cee9522bd2e7e4880fdd0bca
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-03-10 17:11:23 +0100

    Clock decoupled

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 def main():
     print("Starting Asteroids!")
     counts = pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -14,6 +18,7 @@ def main():
                 return
         screen.fill(color="#000000")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
