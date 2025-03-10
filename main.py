a6cbad3e96a5a7d7acbb364bec0fe3a47c391f3d
FG-GIS <flore.giovanni.scripting@outlook.com>
2025-03-10 18:42:32 +0100

    Moving starship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import player as pl
 from constants import *
 
 def main():
@@ -12,11 +13,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = pl.Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="#000000")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
